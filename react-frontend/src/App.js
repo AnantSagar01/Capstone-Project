8ce4aa75dccacff21e2ab83caa50e37951b076d6
Sakshi <saku40341@gmail.com>
2025-05-07 14:32:44 +0530

    added

--- react-frontend/src/App.js
+++ react-frontend/src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route ,Navigate} from 'react-router-dom';
 import Home from './Home';
 import LoginForm from './LoginForm';
 import RegisterForm from './RegisterForm';
@@ -11,22 +11,51 @@ import Navbar from './Navbar';
 import Footer from './Footer';
 import './ShopEasy.css';
  
+// function App() {
+//   return (
+//     <Router>
+//       <Navbar />
+//       <Routes>
+//         <Route path="/" element={<Home />} />
+//         <Route path="/login" element={<LoginForm />} />
+//         <Route path="/register" element={<RegisterForm />} />
+//         <Route path="/products" element={<ProductsPage />} />
+//         <Route path="/cart" element={<CartPage />} />
+//         <Route path="/checkout" element={<CheckoutPage />} />
+//         <Route path="/feedback" element={<FeedbackPage />} />
+//       </Routes>
+//       <Footer />
+//     </Router>
+//   );
+// }
+const isAuthenticated = () => {
+  return localStorage.getItem('token') !== null;
+};
 function App() {
   return (
     <Router>
-      <Navbar />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/login" element={<LoginForm />} />
-        <Route path="/register" element={<RegisterForm />} />
-        <Route path="/products" element={<ProductsPage />} />
-        <Route path="/cart" element={<CartPage />} />
-        <Route path="/checkout" element={<CheckoutPage />} />
-        <Route path="/feedback" element={<FeedbackPage />} />
-      </Routes>
-      <Footer />
+      <div>
+        <Navbar />
+
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<LoginForm />} />
+          <Route path="/register" element={<RegisterForm />} />
+          <Route path="/products" element={<ProductsPage />} />
+          <Route path="/cart" element={<CartPage />} />
+          <Route
+            path="/checkout"
+            element={isAuthenticated() ? <CheckoutPage /> : <Navigate to="/login" />}
+          />
+          <Route path="/feedback" element={<FeedbackPage />} />
+          {/* <Route path="*" element={<NotFoundPage />} /> */}
+        </Routes>
+
+        <Footer />
+      </div>
     </Router>
   );
 }
+
  
 export default App;
