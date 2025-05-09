5dc262009ee6b48bc0a17404442a42c1740cf79c
ANANT SAGAR <92757910+AnantSagar01@users.noreply.github.com>
2025-05-09 13:25:59 +0530

    Delete product-service-spring_boot directory

--- product-service-spring_boot/productDb/src/main/java/com/spring_boot/productDb/ProductRepository/ProductRepository.java
+++
@@ -1,9 +0,0 @@
-package com.spring_boot.productDb.ProductRepository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.spring_boot.productDb.Product;
-
-public interface ProductRepository extends JpaRepository<Product, Long> {
-
-}
