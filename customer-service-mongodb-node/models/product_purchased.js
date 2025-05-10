5e967436ad76c8ad43d0f636619cbebb25196bd7
ANANT SAGAR <92757910+AnantSagar01@users.noreply.github.com>
2025-05-10 23:49:15 +0530

    Update product_purchased.js

--- customer-service-mongodb-node/models/product_purchased.js
+++ customer-service-mongodb-node/models/product_purchased.js
@@ -1,12 +1,12 @@
 const mongoose = require('mongoose');
  
 const UserSchema = new mongoose.Schema({
-    purchaseHistory: [
-        {
-            productId: { type: String },
-            purchaseDate: { type: Date, default: Date.now }
-        }
-    ]
+  purchaseHistory: [
+    {
+      productId: { type: String },
+      purchaseDate: { type: Date, default: Date.now }
+    }
+  ]
 });
- 
-module.exports = mongoose.model('User', UserSchema);    
+
+module.exports = mongoose.model('User', UserSchema);  
