3338342b7ce3e6ed7b610d69c924b30072809156
ANANT SAGAR <92757910+AnantSagar01@users.noreply.github.com>
2025-05-10 23:50:09 +0530

    Update regitser.js

--- customer-service-mongodb-node/models/regitser.js
+++ customer-service-mongodb-node/models/regitser.js
@@ -1,12 +1,14 @@
 const mongoose = require('mongoose');
  
 const UserSchema = new mongoose.Schema({
-  firstname: { type: String},
-  lastname: { type: String},
-  email: { type: String, required: true, unique: true },
-  password: { type: String},
-  confirm_password: {type: String}
+ firstname: { type: String},
+ lastname: { type: String},
+ email: { type: String, required: true, unique: true },
+ password: { type: String},
+ confirm_password: {type: String}
 });
- 
-module.exports = mongoose.model('User', UserSchema);
- 
+
+// module.exports = mongoose.model('User', UserSchema);
+const User = mongoose.model('User', UserSchema);
+
+module.exports = User;
